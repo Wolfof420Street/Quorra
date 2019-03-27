56e70b931311e54d871d17f3e5c44f4862002071
Wolfof420Street <billodida420@gmail.com>
2019-03-27 12:05:16 +0300

    creating an http service

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,9 +2,12 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+
 export const environment = {
-  production: false
+  production: false,
+  apiUrl:"http://quotes.stormconsultancy.co.uk/random.json (Links to an external site.)Links to an external site."
 };
+
 
 /*
  * For easier debugging in development mode, you can import the following file
